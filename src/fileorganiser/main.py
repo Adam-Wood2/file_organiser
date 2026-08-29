00ce0f01fc7b630f3a31a0dc422d154dd666b2e1
unknown <adamowod2007@gmail.com>
2026-08-29 20:54:39 +0100

    Added method to File class that lets it retrieve its destination folder from the extension_lookup dict. Added a function called scan_dir which iterates through all files in the directory and adds any not ignored files to a list. It also returns a list of folders that will need to be created. Added an attribute to File objects called is_ignored which checks if the files extension is in the ignore list in the config file. Added function that prints a progress bar.

--- src/fileorganiser/main.py
+++ src/fileorganiser/main.py
@@ -1,15 +1,17 @@
 import os
 from configparser import ConfigParser
+from time import sleep
 
 class File:
-    def __init__(self, file_name,location):
+    def __init__(self, file_name, location):
         self.file_name = file_name
         self.location = location
         self.full_path = os.path.join(self.location, self.file_name)
         self.file_extension = self.__get_file_extension()
         self.file_type = self.__get_file_type()
         self.size = self.get_file_size()
-
+        self.is_ignored = self.file_extension in ignore_lookup
+        self.destination = self.get_destination()
     
     def format_size(self):
         units = ["B","KB","MB","GB"]
@@ -25,13 +27,36 @@ class File:
         return "." + self.file_name.split(".")[-1]
 
     def __get_file_type(self):
-        return extension_lookup[self.file_extension]
-
+        if self.file_extension in ignore_lookup:
+            return self.file_extension
+        elif self.file_extension in extension_lookup:
+            return extension_lookup[self.file_extension]
+        else:
+            return "Other"
+        
     def get_file_size(self):
         stats = os.stat(self.full_path)
         size = stats.st_size
         return size
 
+    def get_destination(self):
+        if self.file_extension in ignore_lookup:
+            return "N/A"
+        elif self.file_extension in extension_lookup:
+            return extension_lookup[self.file_extension]
+        else:
+            return "Other"
+
+
+def draw_progress_bar(val1, val2):
+
+    progress = int(((val1 / val2)*100) // 2)
+    progress_bar = "[" + "\u001b[47m.\u001b[0m"*progress + "."*(50-progress) + "]" + f" {val1}/{val2}"
+    #return progress_bar
+    if val1 != val2:
+        print(progress_bar, end="\r")
+    else:
+        print(progress_bar)
 #C:\Users\Adam\OneDrive\Documents\Programs\Test Folder
 #Gets the directory from the user and extracts all files and folders from it
 def get_directories():
@@ -62,12 +87,35 @@ def load_configs():
 
 def generate_lookup(folders_config):
     extension_lookup = {}
+    ignore_lookup = []
     for file_type, extensions in folders_config.items():
         for extension in extensions:
-            extension_lookup[extension] = file_type
-    return extension_lookup
+            if file_type != "ignore":
+                extension_lookup[extension] = file_type
+            else:
+                ignore_lookup.append(extension)
+    return extension_lookup, ignore_lookup
 
+#Scans through all directories in the given location to get all the files within. Returns a list of all existing files
+#and of which folders will be needed.
+def scan_dir(dir, dir_files, dir_folders):
 
+    needed_folders = []
+    files = []
+    total_files = len(dir_files)
+    current_file_num = 0
+    for file_name in dir_files:
+        file = File(file_name, dir)
+
+        file_destination = file.get_destination()
+
+        if not file.is_ignored and file_destination not in needed_folders and file_destination not in dir_folders:
+            needed_folders.append(file_destination)
+        files.append(file)
+        current_file_num += 1
+        draw_progress_bar(current_file_num,total_files)
+
+    return files, needed_folders
 
 def main():
     dir, dir_files, dir_folders = get_directories()
@@ -75,11 +123,12 @@ def main():
     #print(dir_files)
     #print(dir_folders)
     #print(folders_config)
-    file = File("test.txt", dir)
-    print(file.format_size())
+    #file = File("test.png", dir)
+
+    
 
 folders_config = load_configs()
-extension_lookup = generate_lookup(folders_config)
+extension_lookup, ignore_lookup = generate_lookup(folders_config)
 
 if __name__ == "__main__":
     main()
